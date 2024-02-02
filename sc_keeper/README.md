c629772a909c4c6b1466f9df713ec45ab68ad94c
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2024-02-02 23:04:42 +0100

    POC FastAPI to look up an instance type and search instance types by cpu/ram

---
+++ sc_keeper/README.md
@@ -0,0 +1,5 @@
+## Useful debug links
+
+- http://localhost:8000/docs
+- http://localhost:8000/server/p3.8xlarge
+- http://localhost:8000/search?vcpus_min=200&vcpus_max=250
---
+++ sc_keeper/main.py
@@ -0,0 +1,47 @@
+from contextlib import asynccontextmanager
+from typing import List, Optional
+
+from fastapi import FastAPI, HTTPException
+from sc_crawler.schemas import Server
+from sqlmodel import Session, SQLModel, create_engine, select
+
+sqlite_file_name = "sc_crawler.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup: init DB
+    SQLModel.metadata.create_all(engine)
+    yield
+    # shutdown
+    pass
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/server/{server_id}", response_model=Server)
+def read_server(server_id: str):
+    with Session(engine) as session:
+        query = select(Server).where(Server.id == server_id)
+        server = session.exec(query).first()
+        if not server:
+            raise HTTPException(status_code=404, detail="Server not found")
+        return server
+
+
+@app.get("/search", response_model=List[Server])
+def search_server(vcpus_min: Optional[int] = None, vcpus_max: Optional[int] = None):
+    with Session(engine) as session:
+        query = select(Server)
+        if vcpus_min:
+            query = query.where(Server.vcpus >= vcpus_min)
+        if vcpus_max:
+            query = query.where(Server.vcpus <= vcpus_max)
+        servers = session.exec(query).all()
+        return servers
+
+
+## https://fastapi-filter.netlify.app/#examples
