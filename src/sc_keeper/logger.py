6e5274ab8b0481c7dd6c7f53df3b4889be53e7d0
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-04-17 16:45:16 +0200

    always record timestamp

--- src/sc_keeper/logger.py
+++ src/sc_keeper/logger.py
@@ -22,13 +22,15 @@ class JsonFormatter(Formatter):
         super(JsonFormatter, self).__init__()
 
     def format(self, record):
-        json_record = {}
-        json_record["message"] = record.getMessage()
-        json_record["level"] = record.__dict__["levelname"]
-        json_record["caller"] = {
-            k: record.__dict__[k]
-            for k in ["lineno", "module", "logger"]
-            if k in record.__dict__
+        json_record = {
+            "timestamp": time(),
+            "message": record.getMessage(),
+            "level": record.__dict__["levelname"],
+            "caller": {
+                k: record.__dict__[k]
+                for k in ["lineno", "module", "logger"]
+                if k in record.__dict__
+            },
         }
         for nested in ["event", "request_id", "client", "req", "res", "timing"]:
             if nested in record.__dict__:
