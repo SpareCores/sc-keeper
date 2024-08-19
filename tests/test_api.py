05b716a7af6f3bd97c23050cf5bc857d70d24880
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-08-19 23:37:05 +0200

    parametrize test params

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 from time import time
 
+import pytest
 from fastapi.testclient import TestClient
 
 from sc_keeper.api import app
@@ -15,41 +16,48 @@ def test_healthcheck():
     assert response.json()["database_last_updated"] < time()
 
 
-def test_server_prices():
-    for params in [
-        {},
-        {"partial_name_or_id": "cx"},
-        {"vcpus_min": 32},
-        {"architecture": "x86_64"},
-        {"benchmark_score_stressng_cpu_min": 5e5},
-        {"memory_min": 32000},
-        {"price_max": 1},
-        {"green_energy": True},
-        {"allocation": "spot"},
-        {"vendor": ["hcloud"]},
-        {"vendor": ["hcloud", "gcp"]},
-        {"regions": ["us-west-2"]},
-        {"compliance_framework": ["hipaa"]},
-        {"storage_size": 100},
-        {"storage_type": "ssd"},
-        {"countries": ["DE"]},
-        {"gpu_min": 1},
-        {"gpu_memory_min": 1},
-        {"gpu_memory_total": 128},
-    ]:
-        response = client.get(
-            "/server_prices", params=params | {"add_total_count_header": True}
-        )
-        print(params, int(response.headers["x-total-count"]))  # QQ
-        # expect OK status within a reasonable time
-        assert response.status_code == 200
-        assert response.elapsed.total_seconds() < 5
+test_server_prices_params = [
+    {},
+    {"partial_name_or_id": "cx"},
+    {"vcpus_min": 32},
+    {"architecture": "x86_64"},
+    {"benchmark_score_stressng_cpu_min": 5e5},
+    {"memory_min": 32000},
+    {"price_max": 1},
+    {"green_energy": True},
+    {"allocation": "spot"},
+    {"vendor": ["hcloud"]},
+    {"vendor": ["hcloud", "gcp"]},
+    {"regions": ["us-west-2"]},
+    {"compliance_framework": ["hipaa"]},
+    {"storage_size": 100},
+    {"storage_type": "ssd"},
+    {"countries": ["DE"]},
+    {"gpu_min": 1},
+    {"gpu_memory_min": 1},
+    {"gpu_memory_total": 128},
+]
+
+
+@pytest.mark.parametrize("params", test_server_prices_params)
+def test_server_prices_with_params(params):
+    response = client.get(
+        "/server_prices", params=params | {"add_total_count_header": True}
+    )
+    # expect OK status within a reasonable time
+    assert response.status_code == 200
+    assert response.elapsed.total_seconds() < 5
+    # if params is empty, this is the full count
+    if params == {}:
+        global count
+        count = int(response.headers["x-total-count"])
+    else:
         # filtered list should have fewer items than full search
-        if not params:
-            count = int(response.headers["x-total-count"])
-        else:
-            assert int(response.headers["x-total-count"]) < count
-    # only active is set to True by default, so we should find more servers now
+        assert int(response.headers["x-total-count"]) < count
+
+
+def test_server_prices_with_inactive():
+    # only_active is set to True by default, so we should find more servers now
     params = {"only_active": False}
     response = client.get(
         "/server_prices", params=params | {"add_total_count_header": True}
