e5a005feef5dc2bc1e97371e5c71e944c108596e
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-08-19 21:54:39 +0200

    test a few params for search server prices

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,3 +13,15 @@ def test_healthcheck():
     # updated recently
     assert response.json()["database_last_updated"] > time() - 60
     assert response.json()["database_last_updated"] < time()
+
+
+def test_server_prices():
+    for params in [
+        {},
+        {"partial_name_or_id": "cx"},
+        {"vcpus_min": 32},
+        {"architecture": "x86_64"},
+    ]:
+        response = client.get("/server_prices", params=params)
+        assert response.status_code == 200
+        assert response.elapsed.total_seconds() < 5
