a3ee5dfa90881d816719845cca78512f7cb037d0
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-08-20 23:27:14 +0200

    test /servers endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,27 +16,45 @@ def test_healthcheck():
     assert response.json()["database_last_updated"] < time()
 
 
-test_server_prices_params = [
+test_servers_params = [
     {},
     {"partial_name_or_id": "cx"},
     {"vcpus_min": 32},
     {"architecture": "x86_64"},
     {"benchmark_score_stressng_cpu_min": 5e5},
     {"memory_min": 32000},
-    {"price_max": 1},
-    {"green_energy": True},
-    {"allocation": "spot"},
     {"vendor": ["hcloud"]},
     {"vendor": ["hcloud", "gcp"]},
-    {"regions": ["us-west-2"]},
     {"compliance_framework": ["hipaa"]},
     {"storage_size": 100},
     {"storage_type": "ssd"},
-    {"countries": ["DE"]},
     {"gpu_min": 1},
     {"gpu_memory_min": 1},
     {"gpu_memory_total": 128},
 ]
+
+test_server_prices_params = test_servers_params + [
+    {"price_max": 1},
+    {"green_energy": True},
+    {"allocation": "spot"},
+    {"regions": ["us-west-2"]},
+    {"countries": ["DE"]},
+]
+
+
+@pytest.mark.parametrize("params", test_servers_params)
+def test_servers_with_params(params):
+    response = client.get("/servers", params=params | {"add_total_count_header": True})
+    # expect OK status within a reasonable time
+    assert response.status_code == 200
+    assert response.elapsed.total_seconds() < 5
+    # if params is empty, this is the full count
+    if params == {}:
+        global count
+        count = int(response.headers["x-total-count"])
+    else:
+        # filtered list should have fewer items than full search
+        assert int(response.headers["x-total-count"]) < count
 
 
 @pytest.mark.parametrize("params", test_server_prices_params)
