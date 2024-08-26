d46dcc817a1e56f7dfab6d94f28c9e533d5702df
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-08-26 22:15:30 +0200

    drop duckdb in favor of indexed sqlite due to slow full-row lookups

--- src/sc_keeper/database.py
+++ src/sc_keeper/database.py
@@ -2,11 +2,8 @@ from os import environ
 from os.path import abspath
 from time import time
 
-from duckdb import default_connection
 from sc_data import db
-from sqlmodel import Session, create_engine
-
-default_connection.execute("INSTALL sqlite")
+from sqlmodel import Session, create_engine, text
 
 
 class Database:
@@ -21,10 +18,23 @@ class Database:
             self.db_hash = db.hash
             self.last_updated = time()
             self.engine = create_engine(
-                "duckdb:///" + abspath(db.path),
-                connect_args={"preload_extensions": ["sqlite"], "read_only": True},
+                "sqlite:///" + abspath(db.path),
+                connect_args={"check_same_thread": False},
                 echo=bool(environ.get("KEEPER_DEBUG", False)),
             )
+            with self.engine.connect() as conn:
+                for index_create in [
+                    "CREATE INDEX IF NOT EXISTS server_price_idx_4be28cc1 ON server_price(vendor_id, price)",
+                    "CREATE INDEX IF NOT EXISTS server_price_idx_dd929fc9 ON server_price(vendor_id, server_id)",
+                    "CREATE INDEX IF NOT EXISTS server_price_idx_6f0ddbb8 ON server_price(vendor_id, server_id, price)",
+                    "CREATE INDEX IF NOT EXISTS server_price_idx_3902126d ON server_price(server_id, vendor_id, region_id)",
+                    "CREATE INDEX IF NOT EXISTS server_price_idx_f4994df3 ON server_price(allocation, vendor_id, server_id)",
+                    "CREATE INDEX IF NOT EXISTS server_idx_68282de9 ON server(status, server_id, vendor_id)",
+                    "CREATE INDEX IF NOT EXISTS benchmark_score_idx_979d2124 ON benchmark_score(benchmark_id, vendor_id, server_id)",
+                    "VACUUM",
+                ]:
+                    conn.execute(text(index_create))
+
         return Session(autocommit=False, autoflush=False, bind=self.engine)
 
 
