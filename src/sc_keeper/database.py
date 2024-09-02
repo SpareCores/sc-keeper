f561c2ab9495d02d72c660bffbdc7dcefc1fc0af
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-09-02 14:28:56 +0200

    add currency table

--- src/sc_keeper/database.py
+++ src/sc_keeper/database.py
@@ -7,7 +7,7 @@ from sc_data import db
 from sqlmodel import Session, create_engine, delete, insert, text
 
 from .indexes import indexes
-from .views import ServerPriceMin
+from . import views
 
 
 class Database:
@@ -33,17 +33,20 @@ class Database:
                     for index in indexes:
                         index.create(bind=conn, checkfirst=True)
                     # prep and fill ~materialized views
-                    for t in [ServerPriceMin]:
+                    for t in views.views:
                         t.__table__.create(self.engine, checkfirst=True)
-                        conn.execute(delete(t))
-                        q = insert(t).from_select(
-                            t.get_columns()["all"],
-                            # need to instantiate the class to access the private attr
-                            t()._query,
-                        )
-                        conn.execute(q)
-                    # clean up and commit
-                    conn.commit()
+                        if hasattr(t, "insert"):
+                            with Session(self.engine) as session:
+                                t.insert(session)
+                                session.commit()
+                        if hasattr(t, "query"):
+                            conn.execute(delete(t))
+                            q = insert(t).from_select(
+                                t.get_columns()["all"],
+                                t.query(),
+                            )
+                            conn.execute(q)
+                            conn.commit()
                     conn.execute(text("VACUUM"))
 
         return Session(autocommit=False, autoflush=False, bind=self.engine)
--- src/sc_keeper/views.py
+++ src/sc_keeper/views.py
@@ -1,9 +1,34 @@
-from typing import Optional
+from typing import List, Optional
 
-from sc_crawler.table_bases import HasServerPK, HasVendorPKFK
+from sc_crawler.table_bases import HasServerPK, HasVendorPKFK, ScModel
 from sc_crawler.table_fields import Allocation, Status
-from sc_crawler.tables import ServerPrice
-from sqlmodel import case, func, select
+from sc_crawler.tables import ServerPrice, is_table
+from sc_crawler.insert import insert_items
+from sqlmodel import Field, Session, case, func, select
+
+
+from .currency import currency_converter as cc
+
+
+class CurrencyBase(ScModel):
+    base: str = Field(primary_key=True, description="Base currency, e.g. USD.")
+    quote: str = Field(primary_key=True, description="Quote currency, e.g. HUF.")
+    rate: float = Field(description="Exchange rate of base and quote currencies.")
+
+
+class Currency(CurrencyBase, table=True):
+    """Currency symbol pairs exchange rates."""
+
+    @classmethod
+    def insert(self, session: Session):
+        currencies = cc.converter.currencies
+        items = []
+        for base in currencies:
+            for quote in currencies:
+                items.append(
+                    {"base": base, "quote": quote, "rate": cc.convert(1, base, quote)}
+                )
+        insert_items(self, items, session=session)
 
 
 class ServerPriceMinBase(HasServerPK, HasVendorPKFK):
@@ -15,24 +40,31 @@ class ServerPriceMinBase(HasServerPK, HasVendorPKFK):
 class ServerPriceMin(ServerPriceMinBase, table=True):
     """Poor man's materialized view on min price of servers."""
 
-    _query = (
-        select(
-            ServerPrice.vendor_id,
-            ServerPrice.server_id,
-            func.min(ServerPrice.price).label("min_price"),
-            func.min(
-                case((ServerPrice.allocation == Allocation.SPOT, ServerPrice.price))
-            ).label("min_spot_price"),
-            func.min(
-                case(
-                    (
-                        ServerPrice.allocation == Allocation.ONDEMAND,
-                        ServerPrice.price,
+    @staticmethod
+    def query():
+        return (
+            select(
+                ServerPrice.vendor_id,
+                ServerPrice.server_id,
+                func.min(ServerPrice.price).label("min_price"),
+                func.min(
+                    case((ServerPrice.allocation == Allocation.SPOT, ServerPrice.price))
+                ).label("min_spot_price"),
+                func.min(
+                    case(
+                        (
+                            ServerPrice.allocation == Allocation.ONDEMAND,
+                            ServerPrice.price,
+                        )
                     )
-                )
-            ).label("min_ondemand_price"),
+                ).label("min_ondemand_price"),
+            )
+            .where(ServerPrice.status == Status.ACTIVE)
+            .group_by(ServerPrice.vendor_id, ServerPrice.server_id)
+            .order_by(ServerPrice.vendor_id, ServerPrice.server_id)
         )
-        .where(ServerPrice.status == Status.ACTIVE)
-        .group_by(ServerPrice.vendor_id, ServerPrice.server_id)
-        .order_by(ServerPrice.vendor_id, ServerPrice.server_id)
-    )
+
+
+views: List[ScModel] = [
+    o for o in globals().values() if is_table(o) and o.__module__ == "sc_keeper.views"
+]
