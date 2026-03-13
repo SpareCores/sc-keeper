97300481922d1862ede8dd32f0f660450ae2089d
Tamás Hidvégi <105059295+bridgend84@users.noreply.github.com>
2026-03-13 08:12:29 +0100

    Rewrite target to baseline server in src/sc_keeper/parameters.py
    Co-authored-by: Gergely Daróczi <daroczig@rapporter.net>

--- src/sc_keeper/parameters.py
+++ src/sc_keeper/parameters.py
@@ -215,7 +215,7 @@ server_region = Annotated[
     Optional[str],
     Query(
         title="Server region",
-        description="Region of the target server, used for score_per_price ordering to find servers with a similar score_per_price.",
+        description="Region of the baseline server, used for score_per_price ordering to find servers with a similar score_per_price.",
         json_schema_extra={
             "category_id": FilterCategories.REGION,
             "enum": [m.value for m in Regions],
