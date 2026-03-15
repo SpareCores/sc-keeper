8c5468c7c9847effd9403fdf1b42ba2a8206df78
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2026-03-15 21:47:44 +0100

    document new queries

--- src/sc_keeper/queries.py
+++ src/sc_keeper/queries.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from sc_crawler.tables import Allocation, BenchmarkScore, Region, ServerPrice, Status
+from sqlalchemy import Subquery
 from sqlmodel import String, case, func, select
 
 from .helpers import vendor_region_filter
@@ -12,7 +13,20 @@ def gen_live_price_query(
     countries: Optional[countries] = None,
     regions: Optional[regions] = None,
     vendor_regions: Optional[vendor_regions] = None,
-):
+) -> Optional[Subquery]:
+    """Generate a subquery for the live prices of servers in USD.
+
+    Join with this live lookup when the best global prices from ServerExtra are not enough,
+    and you need country/region/vendor-specific best ondemand/spot/monthly etc prices.
+
+    Args:
+        countries: Optional[countries]: The list of country IDs to filter the server prices by.
+        regions: Optional[regions]: The list of region IDs to filter the server prices by.
+        vendor_regions: Optional[vendor_regions]: The list of vendor and region ID pairs separated by a tilde (~) to filter the server prices by.
+
+    Returns:
+        A subquery or None if no filters are provided.
+    """
     if not (countries or regions or vendor_regions):
         return None
     lp = (
@@ -67,7 +81,17 @@ def gen_live_price_query(
     return lp.group_by(ServerPrice.vendor_id, ServerPrice.server_id).subquery()
 
 
-def gen_benchmark_query(benchmark_id: str, benchmark_config: Optional[str] = None):
+def gen_benchmark_query(
+    benchmark_id: str, benchmark_config: Optional[str] = None
+) -> Optional[Subquery]:
+    """Generate a subquery for the filtered view of max benchmark scores of servers.
+
+    Use this subquery when you need to custom benchmark scores e.g. for ordering instead of the global ServerExtra.score.
+
+    Args:
+        benchmark_id: The ID of the benchmark to filter the benchmark scores by.
+        benchmark_config: Optional[str]: The configuration of the benchmark to filter the benchmark scores by.
+    """
     query = select(
         BenchmarkScore.server_id,
         BenchmarkScore.vendor_id,
